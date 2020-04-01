b2a54db7740b595f9a6c44f35cba3ec60d6cc988
Craig Rushforth <craig.rushforth@robotsandpencils.com>
2020-04-01 13:42:58 -0600

    Initial commit

---
+++ PasswordAutofill/PasswordAutofill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PasswordAutofill
+//
+//  Created by Craig Rushforth on 2020-04-01.
+//  Copyright © 2020 Craig Rushforth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
